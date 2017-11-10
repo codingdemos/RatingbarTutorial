50076a6cf74ef19247956361e6b1dac5ffcd4c55
CodingDemos <codingdemos@gmail.com>
2017-11-10 08:38:57 +0800

    First commit

---
+++ app/src/main/java/com/codingdemos/ratingbartutorial/MainActivity.java
@@ -0,0 +1,69 @@
+package com.codingdemos.ratingbartutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RatingBar mRatingBar;
+    TextView mRatingScale;
+    EditText mFeedback;
+    Button mSendFeedback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+        mRatingScale = (TextView) findViewById(R.id.tvRatingScale);
+        mFeedback = (EditText) findViewById(R.id.etFeedback);
+        mSendFeedback = (Button) findViewById(R.id.btnSubmit);
+
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                mRatingScale.setText(String.valueOf(v));
+                switch ((int) ratingBar.getRating()) {
+                    case 1:
+                        mRatingScale.setText("Very bad");
+                        break;
+                    case 2:
+                        mRatingScale.setText("Need some improvement");
+                        break;
+                    case 3:
+                        mRatingScale.setText("Good");
+                        break;
+                    case 4:
+                        mRatingScale.setText("Great");
+                        break;
+                    case 5:
+                        mRatingScale.setText("Awesome. I love it");
+                        break;
+                    default:
+                        mRatingScale.setText("");
+                }
+            }
+        });
+
+        mSendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mFeedback.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please fill in feedback text box", Toast.LENGTH_LONG).show();
+                } else {
+                    mFeedback.setText("");
+                    mRatingBar.setRating(0);
+                    Toast.makeText(MainActivity.this, "Thank you for sharing your feedback", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
